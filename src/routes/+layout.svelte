8e3dc29db3d7f4488b52ba7e5fc9174c41678a25
Neobrazovannyy <friedrichnietzsche32dot@gmail.com>
2025-06-26 14:46:31 +0300

    create project | welcome_page (v_1)

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,114 @@
+<script lang="ts">
+    let { children } = $props();
+</script>
+
+<!-- Window Tabs -->
+<div class="window_tabs">
+
+  <div class="left_window_tabs">
+    left Window Tabs
+  </div>
+
+  <div class="right_window_tabs">
+    right Window Tabs
+  </div>
+
+</div>
+
+<!-- Main Window & Size Menu -->
+<main>
+  <!-- Size Menu -->
+  <div class="size_menu">
+
+    <div class="side_tabs">
+      side tabs
+    </div>
+    
+    <div class="collapsible_tab">
+      collapsible tab
+    </div>
+
+  </div>
+
+  <!-- Main Window -->
+  <div class="main_window">
+    <!-- Wintertale -->
+    {@render children()}
+  </div>
+</main>
+
+<!-- Footer -->
+<div class="footer">
+  Footer
+</div>
+
+<style lang="scss">
+  // font
+  $absolute_font_family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  $absolute_font_weight: 300;
+  // color
+  $lite_blue_snow: rgb(63, 85, 118);  
+  $dark_snow: rgb(40, 54, 75);
+  $lite_color: rgb(200, 229, 255);
+  $text_white: rgb(229, 242, 255);  // text
+  // other const
+  $width_size: 250px;
+
+
+ *{
+    background-color: $dark_snow;
+    color: $text_white;
+    font-size: 16px;
+    font-family: $absolute_font_family;
+    font-weight: $absolute_font_weight;
+    border: 2px solid $lite_color;  /*del*/
+  }
+  
+
+  /*=====================*/
+  /*==== Window Tabs ====*/
+  /*=====================*/
+  .window_tabs{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    height: 30px;
+    font-size: 16px;
+  }
+
+  
+  /*=====================*/
+  /*==== Main Window ====*/
+  /*=====================*/
+  main{
+    display: flex;
+    flex-direction: row;
+    font-size: 50px;
+    flex: 1;
+
+    .size_menu{
+      display: flex;
+      flex-direction: row;
+      width: $width_size;
+
+      .side_tabs{
+        width: 40px;
+      }
+      .collapsible_tab{
+        flex: 1;
+      }
+    }
+
+    .main_window{
+      flex: 1;
+    }
+  }
+  
+  /*================*/
+  /*==== Footer ====*/
+  /*================*/
+  .footer{ 
+    height: 20px;
+    font-size: 16px;
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,122 @@
+<div class="falling_snow"></div>
+
+<div class="welcome_window">
+    <span class="title">
+        welcom
+    </span>
+    <span class="to">
+        to
+    </span>
+    <span class="name">
+        Winter Tale
+    </span>
+</div>
+
+<div class="button">
+    <div class="box_btn box_create">
+        <button class="btn_create">
+            create
+        </button>
+        <span class="text">
+            Create a new note
+        </span>
+    </div>
+    <div class="box_btn box_open">
+        <button class="btn_open">
+            open
+        </button>
+        <span class="text">
+            Open an existing note
+        </span>
+    </div>
+</div>
+
+
+<style lang="scss">
+  // font
+  $absolute_font_family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  $absolute_font_weight: 300;
+  // color
+  $lite_blue_snow: rgb(63, 85, 118);  
+  $dark_snow: rgb(40, 54, 75);
+  $lite_color: rgb(200, 229, 255);
+  $text_white: rgb(229, 242, 255);  // text
+
+
+    .falling_snow{
+        position: absolute;
+    }
+
+    .welcome_window{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 40%;
+        font-size: $absolute_font_family;
+        font-weight: $absolute_font_weight;
+
+        .title{
+            font-size: 50px;
+            text-transform: uppercase;
+        }
+        .to{
+            font-size: 30px;
+        }
+        .name{
+            font-size: 50px;
+        }
+    }
+
+    .button{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        font-size: 25px;
+        text-transform: uppercase;
+
+        button{
+            all: unset;
+            cursor: pointer;
+            background-color: $lite_color;
+            color: $text_white;
+            text-align: center;
+            margin-bottom: 10px;
+            border-radius: 5px;
+        }
+
+
+        .box_btn{
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            border-bottom: 2px solid $lite_color;
+            margin-bottom: 30px;
+            width: 500px;
+        }
+
+        .box_create{
+            .btn_create{
+                width: 120px;
+                height: 45px;
+            }
+
+            .text{
+                font-size: 20px;
+            }
+        }
+        
+        .box_open{
+            flex-direction: row-reverse;
+
+            .btn_open{
+                width: 120px;
+                height: 45px;
+            }
+            .text{
+                font-size: 20px;
+            }
+        }
+    }
+
+</style>
