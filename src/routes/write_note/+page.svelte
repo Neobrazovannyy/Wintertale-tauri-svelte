5867af4441b6297b2227818cc4fd9ffbfaebcd95
Neobrazovannyy <friedrichnietzsche32dot@gmail.com>
2025-06-28 17:11:06 +0300

    work block_write & block_read

--- src/routes/write_note/+page.svelte
+++ src/routes/write_note/+page.svelte
@@ -1,32 +1,58 @@
 <script lang="ts">
-    let if_convert_write: boolean = $state(true);
-    let text_textarea: string = $state("");
+    let m_if_convert_write: boolean = $state(true);
+    let m_text_textarea: string = $state("");
 
-    function ShowBlockRide(): void{
-        if_convert_write=false;
+    function ShowBlockRead(): void{
+        m_if_convert_write=false;
     }
 
     function ShowBlockWrite(): void{
-        if_convert_write=true;
+        m_if_convert_write=true;
     }
 
-    //! doesn't wark
-    function GetTextFromTextarea(): void{
-        let el_textarea = document.querySelector(".block_write") as HTMLTextAreaElement;
-        let text_textarea = el_textarea.value;
+    async function HandleBlurBlockWrite(): Promise<void>{
+        await ShowBlockRead();
+        await SetTextBlockRead();
+    }
+
+    function GetTextFromBlockWrite(): void{
+        let el_block_write = document.querySelector(".block_write") as HTMLTextAreaElement | null;
+        let text_textarea: string | undefined = el_block_write?.value;
+        if(typeof(text_textarea) == "undefined"){
+            m_text_textarea="";
+        }
+        else if(typeof(text_textarea) == "string"){
+            m_text_textarea=text_textarea;
+        }
+    }
+
+    function SetTextBlockRead(): void{
+        let el_block_read = document.querySelector(".block_read") as HTMLDivElement | null;
+        if(el_block_read){
+            // el_block_read.innerHTML=m_text_textarea;
+            el_block_read.innerText=m_text_textarea;
+        }
+    }
+
+    function SetTextBlockWrite(): void{
+        let el_block_write = document.querySelector(".block_write") as HTMLTextAreaElement | null;
+        if(el_block_write){
+            el_block_write.value = m_text_textarea;
+        }
     }
 
 </script>
 
 <div class="record_field">
-    {#if if_convert_write}
+    {#if m_if_convert_write}
         <textarea class="block_write"
-            on:blur={ShowBlockRide}
-            on:input={GetTextFromTextarea}
+            on:focus={SetTextBlockWrite}
+            on:blur={HandleBlurBlockWrite}
+            on:input={GetTextFromBlockWrite}
             autofocus
         ></textarea>
     {:else}
-        <div class="block_rite"
+        <div class="block_read"
             on:click={ShowBlockWrite}
         ></div>
     {/if}
@@ -48,11 +74,12 @@ A4:
         // border
     // $border_lite_blue: #5C7198;
         // background
-    $bg_dark_grey: #161b24; // first choice
-    $bg_dark_blue: #172030; // second choice
+    $bg_dark_grey: #161b24;         // first choice
+    $bg_more_dark_grey: #141820;    // first choice (more dark)
+    $bg_dark_blue: #172030;         // second choice
 
-    *{
-        border: 2px solid #000; //del
+    *{ //del
+        // border: 2px solid #000;
     }
 
     .record_field{
@@ -63,23 +90,34 @@ A4:
         height: 100%;
     }
 
-    @mixin style_block_write_rite{
+    @mixin style_block_write_read{
         font-size: 16px;
         letter-spacing: 1.6px;
         font-family: $absolute_font_family;
         font-weight: $absolute_font_weight;
+        background-color: $bg_more_dark_grey;
         width: 794px;
         height: 100%;
         padding: 5px;
+        margin: 5px;
+        border: 0px;
+        // border: 0px solid $text_and_border_lite_blue;
+        border-radius: 5px;
     }
 
     .block_write{
-        @include style_block_write_rite;
+        @include style_block_write_read;
         resize: none;
     }
 
-    .block_rite{
-        @include style_block_write_rite;
+    .block_read{
+        @include style_block_write_read;
+        word-wrap: break-word;;
+    }
+
+    .block_write:focus{
+        color: $text_white;
+        outline: none;
     }
 
 </style>
